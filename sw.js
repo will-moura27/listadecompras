98c5f972861577f1cc0115e8913560f10109e23c
willi <abismo.nosso@gmail.com>
2026-08-01 20:51:23 -0300

    adicionando logo na versao mobile

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Mudamos a versão para v2. O celular vai perceber essa mudança!
-const CACHE_NAME = 'stoka-pwa-v2'; 
+const CACHE_NAME = 'stoka-pwa-v2.1'; 
 const urlsToCache = [
   './',
   './index.html',
