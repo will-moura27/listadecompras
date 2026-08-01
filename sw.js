f6e6cb472667c0be2703ad297ef67dc788b69da5
willi <abismo.nosso@gmail.com>
2026-08-01 20:42:05 -0300

    adicionando icones pra pagina e app

--- sw.js
+++ sw.js
@@ -1,13 +1,16 @@
-const CACHE_NAME = 'restaurante-pwa-v1';
+// Mudamos a versão para v2. O celular vai perceber essa mudança!
+const CACHE_NAME = 'stoka-pwa-v2'; 
 const urlsToCache = [
   './',
   './index.html',
   './style.css',
-  './script.js'
+  './script.js',
+  './icone.svg'
 ];
 
-// Instala o Service Worker e salva os arquivos no cache
+// Instala a nova versão e força ela a assumir o controle
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
@@ -16,7 +19,24 @@ self.addEventListener('install', event => {
   );
 });
 
-// Intercepta as requisições para carregar rápido do cache
+// Limpa o lixo da versão antiga (Gestão Restaurante v1)
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Apagando cache antigo:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Responde com o cache novo ou busca na rede
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
